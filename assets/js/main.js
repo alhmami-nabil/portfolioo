f6f7efc0508e73adf0020cc6f8a9a6c6f4ac6c8c
alhmami-nabil <nabil.alhmami.solicode@gmail.com>
2021-02-25 12:01:59 +0100

    site

---
+++ assets/js/main.js
@@ -0,0 +1,56 @@
+/*===== MENU SHOW =====*/ 
+
+
+
+/* ================ fuction desplay none and block =====================*/
+
+
+function myFunction1() {
+    document.getElementById("haeder").style.display = "block";
+    document.getElementById("home").style.display = "block";
+    document.getElementById("about").style.display = "none";
+    document.getElementById("skills").style.display = "none";
+    document.getElementById("work").style.display = "none";
+    document.getElementById("contact").style.display = "none";
+    document.getElementById("footer").style.display = "block";
+    }
+    function myFunction2() {
+        document.getElementById("haeder").style.display = "block";
+        document.getElementById("home").style.display = "none";
+        document.getElementById("about").style.display = "block";
+        document.getElementById("skills").style.display = "none";
+        document.getElementById("work").style.display = "none";
+        document.getElementById("contact").style.display = "none";
+        document.getElementById("footer").style.display = "block";
+        }
+        function myFunction3() {
+            document.getElementById("haeder").style.display = "block";
+            document.getElementById("home").style.display = "none";
+            document.getElementById("about").style.display = "none";
+            document.getElementById("skills").style.display = "block";
+            document.getElementById("work").style.display = "none";
+            document.getElementById("contact").style.display = "none";
+            document.getElementById("footer").style.display = "block";
+            }
+            function myFunction4() {
+                document.getElementById("haeder").style.display = "block";
+                document.getElementById("home").style.display = "none";
+                document.getElementById("about").style.display = "none";
+                document.getElementById("skills").style.display = "none";
+                document.getElementById("work").style.display = "block";
+                document.getElementById("contact").style.display = "none";
+                document.getElementById("footer").style.display = "block";
+                }
+                function myFunction5() {
+                    document.getElementById("haeder").style.display = "block";
+                    document.getElementById("home").style.display = "none";
+                    document.getElementById("about").style.display = "none";
+                    document.getElementById("skills").style.display = "none";
+                    document.getElementById("work").style.display = "none";
+                    document.getElementById("contact").style.display = "block";
+                    document.getElementById("footer").style.display = "block";
+                    }
+                
+
+
+
